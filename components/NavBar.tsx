485b0c504b4a5ea00287e985d69f9fbd19a2bec8
Devin Shoemaker <devinshoe@gmail.com>
2021-01-28 19:01:37 -0600

    Make navbar scrollable (#23)

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,7 +2,7 @@ import NavBarLink from './NavBarLink';
 
 function NavBar() {
   return (
-    <nav className="pl-6 h-16 w-full flex items-center bg-gray-900 dark:bg-black shadow-lg z-10 fixed top-0">
+    <nav className="pl-6 h-16 w-full flex items-center bg-gray-900 dark:bg-black shadow-lg z-10 fixed top-0 overflow-auto">
       <NavBarLink label="Home" href="/" />
       <NavBarLink label="Blog" href="/blog" />
       {/* <NavBarLink label="About" href="/about" /> */}
