a533f1a08c22102e2ce91ac259adea1652ab40c5
Devin Shoemaker <devinshoe@gmail.com>
2021-01-28 18:50:29 -0600

    Add footer with copyright

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }) {
   return (
-    <div className="pl-6 pr-6 pt-20 pb-12 lg:max-w-3xl lg:mx-auto">
+    <div className="pl-6 pr-6 pt-24 pb-24 lg:max-w-3xl lg:mx-auto">
       {children}
     </div>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
+import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <div className="relative min-h-screen">
       <NavBar />
       <Component {...pageProps} />
+      <Footer />
     </div>
   );
 }
