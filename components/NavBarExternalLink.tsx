5abe9b6d32b893c9266ffb44046c02bf0082356b
Devin Shoemaker <devinshoe@gmail.com>
2021-02-02 18:01:50 -0600

    Add social links to navbar

---
+++ components/NavBarExternalLink.tsx
@@ -0,0 +1,22 @@
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+interface NavBarExternalLinkProps {
+  href: string;
+  icon: IconProp;
+}
+
+function NavBarExternalLink({ href, icon }: NavBarExternalLinkProps) {
+  return (
+    <a href={href} target="blank" className="ml-6">
+      <FontAwesomeIcon
+        icon={icon}
+        color="white"
+        className="h-8 text-gray-900 dark:text-gray-100"
+      />
+    </a>
+  );
+}
+
+export default NavBarExternalLink;
--- components/NavBarLink.tsx
+++ components/NavBarLink.tsx
@@ -3,21 +3,14 @@ import Link from 'next/link';
 interface NavBarLinkProps {
   href: string;
   label: string;
-  external?: boolean;
 }
 
-function NavBarLink({ label, href, external = false }: NavBarLinkProps) {
+function NavBarLink({ label, href }: NavBarLinkProps) {
   return (
     <ul style={{ height: 'fit-content' }}>
-      {external ? (
-        <a href={href} target="blank" className="mr-4 text-xl text-gray-100">
-          {label}
-        </a>
-      ) : (
-        <Link href={href}>
-          <a className="mr-4 text-xl text-gray-100">{label}</a>
-        </Link>
-      )}
+      <Link href={href}>
+        <a className="mr-4 text-xl text-gray-100">{label}</a>
+      </Link>
     </ul>
   );
 }
