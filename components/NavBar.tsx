759a3a22d44178b14a73afac4dfe4614de10106f
Devin Shoemaker <devinshoe@gmail.com>
2021-02-02 18:06:30 -0600

    Add social links to navbar (#25)

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,17 +1,34 @@
+import {
+  faGithub,
+  faTwitch,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
+import NavBarExternalLink from './NavBarExternalLink';
 import NavBarLink from './NavBarLink';
 
 function NavBar() {
   return (
-    <nav className="pl-6 h-16 w-full flex items-center bg-gray-900 dark:bg-black shadow-lg z-10 fixed top-0 overflow-auto">
-      <NavBarLink label="Home" href="/" />
-      <NavBarLink label="Blog" href="/blog" />
-      <NavBarLink label="About" href="/about" />
-      <NavBarLink
-        label="GitHub"
-        href="https://github.com/devinshoemaker"
-        external={true}
-      />
-    </nav>
+    <div className="flex justify-between items-center px-6 h-16 w-full bg-gray-900 dark:bg-black shadow-lg z-10 fixed top-0 overflow-auto">
+      <nav className="flex">
+        <NavBarLink label="Home" href="/" />
+        <NavBarLink label="Blog" href="/blog" />
+        <NavBarLink label="About" href="/about" />
+      </nav>
+      <div className="flex">
+        <NavBarExternalLink
+          href="https://twitch.tv/paranoidcoder"
+          icon={faTwitch}
+        />
+        <NavBarExternalLink
+          href="https://twitter.com/paranoidcoder"
+          icon={faTwitter}
+        />
+        <NavBarExternalLink
+          href="https://github.com/devinshoemaker"
+          icon={faGithub}
+        />
+      </div>
+    </div>
   );
 }
 
