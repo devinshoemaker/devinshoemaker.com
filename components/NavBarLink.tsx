60459273529d2db1164ac29aca8df6a20362a70b
Devin Shoemaker <devinshoe@gmail.com>
2021-02-02 18:18:58 -0600

    Add underline to anchor tags in global styles

--- components/NavBarLink.tsx
+++ components/NavBarLink.tsx
@@ -9,7 +9,7 @@ function NavBarLink({ label, href }: NavBarLinkProps) {
   return (
     <ul style={{ height: 'fit-content' }}>
       <Link href={href}>
-        <a className="mr-4 text-xl text-gray-100">{label}</a>
+        <a className="mr-4 text-xl text-gray-100 no-underline">{label}</a>
       </Link>
     </ul>
   );
